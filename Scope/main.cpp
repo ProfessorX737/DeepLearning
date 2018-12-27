0227f0ae6867b246e51ff4dfe6b14f9bb11473af
Xavier Poon <>
2018-12-28 09:22:36 +1100

    ignoring vs junk and some test changes in main

--- Scope/main.cpp
+++ Scope/main.cpp
@@ -17,63 +17,70 @@
 
 using namespace std;
 
+//int main(void) {
+//    Graph graph;
+//    auto w1 = Variable(graph,{1,2},DT_FLOAT);
+//    auto w2 = Variable(graph, {1,2}, DT_FLOAT);
+//    auto sqrDiff = Square(graph, Sub(graph,w2,w1));
+//    auto error = Multiply(graph, sqrDiff, 0.5f);
+//    auto optimizer = OptimizerOp<float>(graph,error);
+//    w1->init<float>({1,1});
+//    w2->init<float>({3,3});
+//    
+//    Tensor dx;
+//    optimizer.evalDeriv(dx);
+//    cout << dx.matrix<float>() << endl;
+//    cout << dx.dimString() << endl;
+//	cin.get();
+//    return 0;
+//}
+
 int main(void) {
-    Graph graph;
-    auto w1 = Variable(graph,{1,2},DT_FLOAT);
-    auto w2 = Variable(graph, {1,2}, DT_FLOAT);
-    auto sqrDiff = Square(graph, Sub(graph,w2,w1));
-    auto error = Multiply(graph, sqrDiff, 0.5f);
-    auto optimizer = OptimizerOp<float>(graph,error);
-    w1->init<float>({1,1});
-    w2->init<float>({3,3});
-    
+	const int I = 2;
+	const int H = 2;
+	const int O = 1;
+	const int BATCH_SIZE = 1;
+
+	// get all our data and store into tensors
+	Tensor data({ BATCH_SIZE,I }, DT_FLOAT);
+	Tensor label({ BATCH_SIZE,1 }, DT_FLOAT);
+	data.fill<float>({ 1,1 });
+	label.fill<float>({ 1 });
+
+	Graph graph;
+
+	auto x = Placeholder(graph, { BATCH_SIZE,I });
+	auto y = Placeholder(graph, { BATCH_SIZE,O });
+
+	auto w1 = Variable(graph, { I,H }, DT_FLOAT);
+	auto b1 = Variable(graph, { BATCH_SIZE,H }, DT_FLOAT);
+	auto mult = MatMul(graph, x, w1);
+	auto add = Add(graph, mult, b1);
+	auto h1 = Tanh(graph, add);
+	auto w2 = Variable(graph, { H,O }, DT_FLOAT);
+	auto b2 = Variable(graph, { BATCH_SIZE,O }, DT_FLOAT);
+	auto h2 = Add(graph, MatMul(graph,h1,w2), b2);
+	auto sqrDiff = Square(graph, Sub(graph, h2, y));
+	auto error = Multiply(graph, sqrDiff, 0.5f);
+
+	//w1->init(RandomNormal<float>(0, 0.1));
+	//w2->init(RandomNormal<float>(0, 0.1));
+	w1->init<float>({ 2,2,2,2 });
+	w2->init<float>({ 2,2 });
+	b1->init(ZeroInit<float>());
+	b2->init(ZeroInit<float>());
+
+	//std::vector<Tensor> out;
+ //   Graph::eval({ {x,data},{y,label} }, { h2,sqrDiff }, out);
+	auto optimizer = OptimizerOp<float>(graph, error);
     Tensor dx;
     optimizer.evalDeriv(dx);
     cout << dx.matrix<float>() << endl;
     cout << dx.dimString() << endl;
-    return 0;
-}
 
-//int main(void) {
-//	const int I = 2;
-//	const int H = 2;
-//	const int O = 1;
-//	const int BATCH_SIZE = 1;
-//
-//	// get all our data and store into tensors
-//	Tensor data({ BATCH_SIZE,I }, DT_FLOAT);
-//	Tensor label({ BATCH_SIZE,1 }, DT_FLOAT);
-//	data.fill<float>({ 1,1 });
-//	label.fill<float>({ 1 });
-//
-//	Graph graph;
-//
-//	auto x = Placeholder(graph, { BATCH_SIZE,I });
-//	auto y = Placeholder(graph, { BATCH_SIZE,O });
-//
-//	auto w1 = Variable(graph, { I,H }, DT_FLOAT);
-//	auto b1 = Variable(graph, { BATCH_SIZE,H }, DT_FLOAT);
-//	auto mult = MatMul(graph, x, w1);
-//	auto add = Add(graph, mult, b1);
-//	auto h1 = Tanh(graph, add);
-//	auto w2 = Variable(graph, { H,O }, DT_FLOAT);
-//	auto b2 = Variable(graph, { BATCH_SIZE,O }, DT_FLOAT);
-//	auto h2 = Add(graph, MatMul(graph,h1,w2), b2);
-//	auto sqrDiff = Square(graph, Sub(graph, h2, y));
-//
-//	//w1->init(RandomNormal<float>(0, 0.1));
-//	//w2->init(RandomNormal<float>(0, 0.1));
-//	w1->init<float>({ 2,2,2,2 });
-//	w2->init<float>({ 2,2 });
-//	b1->init(ZeroInit<float>());
-//	b2->init(ZeroInit<float>());
-//
-//	std::vector<Tensor> out;
-//    Graph::eval({ {x,data},{y,label} }, { h2,sqrDiff }, out);
-//
-//	cout << out[0].matrix<float>() << endl;
-//	cout << out[1].matrix<float>() << endl;
-//
-////	cin.get();
-//	return 0;
-//}
+	//cout << out[0].matrix<float>() << endl;
+	//cout << out[1].matrix<float>() << endl;
+
+	cin.get();
+	return 0;
+}
