416d17314d89bb4828f2069c704caa146da61152
Xavier Poon <xavierpoon737@gmail.com>
2019-01-02 10:34:32 +1100

    predicting a merge conflict...

--- Scope/GradientDescent.h
+++ Scope/GradientDescent.h
@@ -1,22 +1,22 @@
-//#pragma once
-//#include "Optimizer.h"
-//#include "Sub.h"
-//
-//template<typename T>
-//class GradientDescentOp : public OptimizerOp<T> {
-//public:
-//	GradientDescentOp(Graph& graph, NodePtr minimize, float learningRate)
-//		: OptimizerOp(graph,minimize), learningRate_(learningRate) {
-//	}
-//
-//	// requires: variables.size() == gradients.size()
-//	void updateVariables(std::vector<Tensor>& variables, const std::vector<Tensor>& gradients) const override {
-//		int nvars = static_cast<int>(variables.size());
-//		CHECK_EQ(nvars, gradients.size());
-//		for (int i = 0; i < nvars; i++) {
-//			SubOp<T>::subtract(variables[i], gradients[i].scalarMult<T>(learningRate_), variables[i]);
-//		}
-//	}
-//private:
-//	float learningRate_;
-//};
+#pragma once
+#include "Optimizer.h"
+#include "Sub.h"
+
+template<typename T>
+class GradientDescentOp : public OptimizerOp<T> {
+public:
+	GradientDescentOp(Graph& graph, NodePtr minimize, float learningRate)
+		: OptimizerOp(graph,minimize), learningRate_(learningRate) {
+	}
+
+	// requires: variables.size() == gradients.size()
+	void updateVariables(std::vector<Tensor>& variables, const std::vector<Tensor>& gradients) const override {
+		int nvars = static_cast<int>(variables.size());
+		CHECK_EQ(nvars, gradients.size());
+		for (int i = 0; i < nvars; i++) {
+			SubOp<T>::subtract(variables[i], gradients[i].scalarMult<T>(learningRate_), variables[i]);
+		}
+	}
+private:
+	float learningRate_;
+};
